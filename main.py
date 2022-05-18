348b52bcaa3dec6a898a88e1be45b86280301d6b
dushyantsingh149 <98822190+dushyantsingh149@users.noreply.github.com>
2022-05-18 21:37:44 +0530

    pep8

--- main.py
+++ main.py
@@ -5,82 +5,61 @@ from hangman_words import word_list
 
 def game(**kargs):
     print(art.logo)
-    
-    stages=art.stages
-    life=len(stages)-1
-    idx=random.randint(0,len(word_list))
-    word=word_list[idx]
-    
-    postnMap={}
-    
-    for idx,letter in enumerate(word):
-        
+
+    stages = art.stages
+    life = len(stages)-1
+    idx = random.randint(0, len(word_list))
+    word = word_list[idx]
+
+    postnMap = {}
+
+    for idx, letter in enumerate(word):
+
         if letter not in postnMap:
-            postnMap[letter]=list()
-            
+            postnMap[letter] = list()
+
         postnMap[letter].append(idx)
-        
-        
-    
-    
-    
-    correctGuess=['_']*len(word)
-    
-    
-    
+
+    correctGuess = ['_']*len(word)
+
     while(True):
         print()
-        guess=str(input('Guess a letter: '))
-        
+        guess = str(input('Guess a letter: '))
+
         if guess in correctGuess:
             print('Its alrady guessed')
             continue
-        
+
         if guess in postnMap:
-            
+
             print('Hurray you guessed correctly keep going')
-            
-            allpos=postnMap[guess]
-            
+
+            allpos = postnMap[guess]
+
             for pos in allpos:
-                correctGuess[pos]=guess
-                
+                correctGuess[pos] = guess
+
         else:
-            life-=1
+            life -= 1
             print('Ohhhh No you lose a life')
-            
-        
+
         print(stages[life])
-        
+
         if life == 0:
             print('You are dead')
             break
-                
-        allDone=True
-        
+
+        allDone = True
+
         for ch in correctGuess:
             if ch == '_':
-                allDone=False
-            
-            print(ch,end=' ')
-            
+                allDone = False
+
+            print(ch, end=' ')
+
         if allDone == True:
             print('You have won')
             break
-            
 
-                    
+
 game()
-    
-            
-            
-           
-                    
-        
-
-    
-    
-    
-    
-
-
